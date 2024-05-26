56685d3766e249d45d2565bb625027b031411b79
guga Apkhazava <aapkhazava22@gmail.com>
2024-05-26 20:02:44 +0400

    Commit Persistance layer

---
+++ AGRB.Optio.Persistance/Reflections/ReflectionServices.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RGBA.Optio.UI.Reflections
+{
+    public static class ReflectionServices
+    {
+        public static void AddInjectServices(this IServiceCollection collection, Assembly assembly, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+        {
+            if (assembly is null)
+            {
+                throw new ArgumentNullException(nameof(assembly), "Assembly cannot be null");
+            }
+
+            var types = assembly.GetTypes().Where(type =>
+                   !type.IsAbstract &&
+                   !type.IsGenericTypeDefinition &&
+                   !type.IsInterface &&
+                   type.Name.Contains("Service", StringComparison.OrdinalIgnoreCase)).ToList();
+
+            foreach (var type in types)
+            {
+                var interfaces = type.GetInterfaces().ToList();
+                if (interfaces.Count == 0)
+                {
+                    continue;
+                }
+
+                foreach (var iface in interfaces)
+                {
+                    collection.AddScoped(iface, type);
+                }
+            }
+        }
+    }
+}
---
+++ AGRB.Optio.Persistance/Reflections/RefrectionRepositories.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace RGBA.Optio.UI.Reflections
+{
+    public static  class RefrectionRepositories
+    {
+        public static void AddInjectRepositories(this IServiceCollection collection, Assembly assembly, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+        {
+            if(assembly is null )
+            {
+                throw new ArgumentNullException(nameof(assembly), "Assembly cannot be null");
+            }
+            var assemb = assembly.GetTypes().Where(i =>
+            !i.IsInterface&&
+            !i.IsAbstract &&
+            !i.IsGenericTypeDefinition &&
+            i.Name.Contains("Repos",StringComparison.OrdinalIgnoreCase)
+            );
+
+            foreach( var type in assemb )
+            {
+                var interfaceRepository = type.GetInterfaces();
+                if(interfaceRepository.Count()==0)
+                {
+                    continue;
+                }
+                foreach (var item in interfaceRepository)
+                {
+                    collection.AddScoped(item, type);
+                }
+            }
+        }
+    }
+}
