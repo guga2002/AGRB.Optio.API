3a07eb2adef7db7f548babd4c632b7b15e2f3f16
RAISA <RAISA@DESKTOP-Q5BH0M9>
2024-05-27 20:26:01 +0400

    Refactoring Domain layer, add Log entity

--- AGRB.Optio.API/Controllers/WeatherForecastController.cs
+++ AGRB.Optio.API/Controllers/WeatherForecastController.cs
@@ -11,11 +11,11 @@ namespace AGRB.Optio.API.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForecastController> logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
-            _logger = logger;
+            this.logger = logger;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
--- AGRB.Optio.Domain/Entities/Category.cs
+++ AGRB.Optio.Domain/Entities/Category.cs
@@ -5,19 +5,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Optio.Core.Entities
 {
     [Table("CategoryOfTransactions")]
-    [Index(nameof(TransactionCategory),IsDescending =new bool[]{true})]
+    [Index(nameof(TransactionCategory), IsDescending = [true])]
     public class Category:AbstractClass
     {
 
         [Column("Transaction_Category")]
-        [StringLength(50,ErrorMessage ="Such Transaction Cattegory is not valid",MinimumLength=4)]
+        [StringLength(50,ErrorMessage ="Such Transaction category is not valid",MinimumLength=4)]
         public required string TransactionCategory { get; set; }
         public bool IsActive { get; set; } = true;
 
         [ForeignKey("typeOfTransaction")]
-        public long TransactionTypeID { get; set; }
+        public long TransactionTypeId { get; set; }
 
-        public TypeOfTransaction typeOfTransaction { get; set; }
+        public TypeOfTransaction TypeOfTransaction { get; set; }
 
         public virtual IEnumerable<Transaction> Transactions { get; set; }
     }
