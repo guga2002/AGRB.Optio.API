34d70e2297452b8579296a437b2f568b62789c71
aapkh <aapkh@GUGAAPKH>
2024-05-29 20:34:23 +0400

    Add: Add static file Named DefaultText

---
+++ AGRB.Optio.API/StaticFiles/DefaultText.cs
@@ -0,0 +1,9 @@
+﻿namespace AGRB.Optio.API.StaticFiles
+{
+    public static class DefaultText
+    {
+        const string notDefined = " This field is not defined";
+        const string noText = "This field  do ot contain any  valuable value";
+        const string noValue = " This is empty string, whith no Valuable value";
+    }
+}
